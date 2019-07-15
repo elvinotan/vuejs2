e354dfa9bf7b861241c5f5f758af0d10b760a93c
elvino <elvino_tan@yahoo.com>
2019-07-15 20:33:37 +0700

    commt

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -1,21 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueResource from 'vue-resource';
-import VueRouter from 'vue-router';
-import Routers from './routers';
+import Vue from "vue";
+import App from "./App.vue";
+import VueResource from "vue-resource";
+import VueRouter from "vue-router";
+import Routers from "./routers";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
+Vue.use(BootstrapVue);
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes: Routers,
-  mode: 'history'
-})
+  mode: "history"
+});
 
 export const bus = new Vue({});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App),
   router: router
-})
+});
